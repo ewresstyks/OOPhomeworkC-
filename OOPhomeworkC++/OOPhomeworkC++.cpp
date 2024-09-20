b14ddb6abe4a9381a5b36cfff6f5b8abd80e1526
margo <margo@lalala>
2024-09-20 16:14:00 +0300

    na pare

--- OOPhomeworkC++/OOPhomeworkC++.cpp
+++ OOPhomeworkC++/OOPhomeworkC++.cpp
@@ -7,17 +7,37 @@ class fraction {
 private:
     int a, b;
 public:
+    int GetA() {
+        return a;
+    }
+    int GetB() {
+        return b;
+    }
+    void SetA(int _a) {
+        a = _a;
+    }
+    void SetB(int _b) {
+        b = _b;
+    }
+
     void Print() {
-        cout << "result a/b -- " << a / b << endl;
+        if (b != 0) {
+            cout << "result a/b -- " << a / b << endl;
+        }
+        else {
+            cout << "error" << endl;
+        }
     }
+
     void Input(int a2, int b2) {
-        a = a2;
-        b = b2;
+        SetA(a2);
+        SetB(b2);
     }
+
     void InputRand() {
         srand(time(0));
-        a = 1 + rand() % (100 - 1 + 1);
-        b = 1 + rand() % (100 - 1 + 1);
+        SetA(1 + rand() % (100 - 1 + 1));
+        SetB(1 + rand() % (100 - 1 + 1));
     }
 };
 
